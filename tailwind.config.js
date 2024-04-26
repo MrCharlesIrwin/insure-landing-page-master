2b6f29c1edda1d3347dac88378833a62cbdf2d26
Charles <116520612d@gmail.com>
2024-04-26 21:14:27 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "color-100": "hsl(256, 26%, 20%)",
+        "color-200": "hsl(270, 9%, 17%)",
+        "color-300": "hsl(273, 4%, 51%)",
+        "color-400": "hsl(216, 30%, 68%)",
+        "color-500": " hsl(0, 0%, 98%)",
+      },
+    },
+  },
+  plugins: [],
+};
